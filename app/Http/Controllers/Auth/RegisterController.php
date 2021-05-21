e149f9d431456a7cd2b5609268051d0d9eb00045
Virgilio Romero <virgilio.romero@luderepro.com>
2021-05-21 00:16:37 -0300

    Agregada autenticacion y tailwindcss

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
+
+class RegisterController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware(['guest']);
+    }
+
+    public function index()
+    {
+        return view('auth.register');
+    }
+
+    public function store(Request $request)
+    {
+        //Validacion, si falla devuelve al form original con los errores
+        $this->validate($request, array(
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|email|max:255',
+            'password' => 'required|confirmed',
+            )
+        );
+
+        //Guardar en Base de Datos
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        //Inicio de sesion
+        auth()->attempt($request->only('email', 'password'));
+
+        //Redireccion al Dashboard
+        return redirect()->route('dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\Auth\RegisterController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home');
+})->name('home');
+
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
+
+Route::get('/register', [RegisterController::class, 'index'])
+        ->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])
+        ->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout', [LogoutController::class, 'store'])
+        ->name('logout');
